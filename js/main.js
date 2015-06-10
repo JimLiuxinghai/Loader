60bca724f13eb901b3931b22c607e4a64b1693c0
jimliu <jimliuxinghai@163.com>
2015-06-10 21:46:59 +0800

    school-first
    require-wrong

--- js/main.js
+++ js/main.js
@@ -26,41 +26,65 @@
 //		});
 //	})
 
+
+//require配置
+require.config(
+    {
+       
+        path:{
+            "jquery":"jquery-1.7.min",
+            "util":"util"
+        },
+        shim : {
+            "util" : {
+                deps : ["jquery"],
+                exports :"util"
+            }
+        }
+    }
+)
+
+
 //根据配置文件生成页面上的按钮 并且添加元素
-	var dataSource="";
-	$.ajax({
-		type:"get",
-		url:"json/menu.json",
-		async:false,
-		success:function(data)
-		{
-			dataSource = data;
-			for(var i=0;i<data.length;i++)
-			{
-				var btn = $("<button></button>").attr("id","btn"+i).html(data[i].value);
-				btn.appendTo($(".btn"));
-			}
-			
-		}
-		
-	});
-	console.log(dataSource);
-	var aBtn = document.getElementsByTagName('button');
-	for(var j=0;j<aBtn.length;j++)
-	{
-		aBtn[j].onclick = function()
-		{
-			var index = j;
-			return function()
-			{
-				util.initJs(dataSource[index].modle);
-				util.initTmpl(dataSource[index].tplURL,function(tmpl)
-				{
-					$('.content').html(tmpl)
-				})
-			}
-		}(j)
-	}
+define(['jquery','util'],function($,util)
+    {
+        var dataSource="";
+        $.ajax({
+            type:"get",
+            url:"json/menu.json",
+            async:false,
+            success:function(data)
+            {
+                dataSource = data;
+                for(var i=0;i<data.length;i++)
+                {
+                    var btn = $("<button></button>").attr("id","btn"+i).html(data[i].value);
+                    btn.appendTo($(".btn"));
+                }
+
+            }
+
+        });
+        console.log(dataSource);
+        var aBtn = document.getElementsByTagName('button');
+        for(var j=0;j<aBtn.length;j++)
+        {
+            aBtn[j].onclick = function()
+            {
+                var index = j;
+                return function()
+                {
+                    util.initJs(dataSource[index].modle);
+                    util.initTmpl(dataSource[index].tplURL,function(tmpl)
+                    {
+                        $('.content').html(tmpl)
+                    })
+                }
+            }(j)
+        }
+    }
+)
+
 	
 	
 	
